ac766c680b15bf11045599eb24d55f6955b646d2
FHP187 <gjimenez@jaumebalmes.net>
2023-03-02 17:56:36 +0100

    Commit practica 14

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coches', function () {
+
+    $cars = DB::table("test_laravel")->get();
+
+    dd($cars);
+
+});
